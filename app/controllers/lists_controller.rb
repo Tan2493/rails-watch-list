a82b709d972905f671b7786c08393d7a4adbefcb
Tania Kwok <tania@pinkmojito.net>
2025-10-23 21:30:02 +0400

    Front updated

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -29,11 +29,11 @@ class ListsController < ApplicationController
 
   private
 
-  def list_params
-    params.require(:list).permit(:name)
-  end
-
   def set_list
     @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name, :image_url)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'lists/index'
+  # get 'lists/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   root to: "lists#index"
-  resources :lists do
+  resources :lists, except: [:edit, :update] do
     resources :bookmarks, only: %i[new create]
   end
   resources :bookmarks, only: [:destroy]
